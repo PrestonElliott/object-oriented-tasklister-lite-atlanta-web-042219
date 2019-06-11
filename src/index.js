464b53a4011a8b5c0ecde237f657114c807102cc
Preston Elliott <preston.elliott24@gmail.com>
2019-06-11 16:07:26 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const taskList = new TaskList()
+  const newTaskForm = document.getElementById("create-task-form")
+  const newTaskDescription = document.getElementById("new-task-description")
+  const listContainer = document.getElementById("list")
 
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const renderApp = () => (listContainer.innerHTML = taskList.render())
 
-  // Add Form Behavior Here!
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    taskList.createNewTask(newTaskDescription.value)
+    e.target.reset()
+    renderApp()
+  })
+
+  listContainer.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description)
+      renderApp()
+    }
+  })
 
   renderApp()
-});
+})
